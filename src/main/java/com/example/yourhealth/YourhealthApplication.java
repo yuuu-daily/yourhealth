1b19c45a089903dd9a16628f82b68e5576d42482
yuichi <y.ichityzy@gmail.com>
2022-03-09 16:09:01 +0900

    first commit

---
+++ src/main/java/com/example/yourhealth/YourhealthApplication.java
@@ -0,0 +1,13 @@
+package com.example.yourhealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourhealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YourhealthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/yourhealth/YourhealthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yourhealth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourhealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
