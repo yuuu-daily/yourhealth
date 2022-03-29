de9363f49b3e8b0a20ff05027374c76b8aaab009
yuichi <y.ichityzy@gmail.com>
2022-03-29 10:49:11 +0900

    make header, footer

--- src/main/java/com/example/yourhealth/controller/RequestParamController.java
+++ src/main/java/com/example/yourhealth/controller/RequestParamController.java
@@ -1,8 +1,10 @@
 package com.example.yourhealth.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.yourhealth.form.Form;
 
@@ -40,10 +42,32 @@ public class RequestParamController {
 	}
 	
 	@GetMapping("weightdate")
-	public String showweightdateView() {
+	public String showWeightdateView() {
 		return "weightdate";
 	}
 	
+	@GetMapping("/contact")
+	public String showContactView() {
+		return "contact";
+	}
 	
+	@GetMapping("/privacyPolicy")
+	public String showprivacyPolicyView() {
+		return "privacyPolicy";
+	}
+	
+    /** contactの確認画面を表示 */
+	@PostMapping("contactConfirm")
+	public String contactConfirmView(
+		Model model, @RequestParam String name, @RequestParam String furigana, 
+		@RequestParam String email, @RequestParam String item, @RequestParam String content
+	) {
+		model.addAttribute("name", name);
+		model.addAttribute("furigana", furigana);
+		model.addAttribute("email", email);
+		model.addAttribute("item", item);
+		model.addAttribute("content", content);
+		return "contactConfirm";
+	}
 	
 }
